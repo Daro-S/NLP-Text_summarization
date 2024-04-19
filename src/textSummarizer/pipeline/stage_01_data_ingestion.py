7fe4855408503d8d6bcd7ca1b616580f4c8f8bf3
Daro <darosim.itdev@gmail.com>
2024-04-19 14:03:10 +0700

    DVC added

--- src/textSummarizer/pipeline/stage_01_data_ingestion.py
+++ src/textSummarizer/pipeline/stage_01_data_ingestion.py
@@ -2,6 +2,9 @@ from textSummarizer.config.configuration import ConfigurationManager
 from textSummarizer.components.data_ingestion import DataIngestion
 from textSummarizer.logging import logger
 
+
+STAGE_NAME = "Data Ingestion stage"
+
 class DataIngestionTrainingPipeline:
     def __init__(self):
         pass
@@ -12,4 +15,14 @@ class DataIngestionTrainingPipeline:
         data_ingestion = DataIngestion(config=data_ingestion_config)
         data_ingestion.download_file()
         data_ingestion.extract_zip_file()
+
+if __name__ == "__main__":
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
         
--- src/textSummarizer/pipeline/stage_03_data_transformation.py
+++ src/textSummarizer/pipeline/stage_03_data_transformation.py
@@ -1,25 +1,25 @@
 from textSummarizer.config.configuration import ConfigurationManager
-from textSummarizer.components.data_validation import DataValiadtion
+from textSummarizer.components.data_transformation import DataTransformation
 from textSummarizer.logging import logger
 
 
-STAGE_NAME = "Data Validation stage"
-
-class DataValidationTrainingPipeline:
+class DataTransformationTrainingPipeline:
     def __init__(self):
         pass
 
     def main(self):
         config = ConfigurationManager()
-        data_validation_config = config.get_data_validation_config()
-        data_validation = DataValiadtion(config=data_validation_config)
-        data_validation.validate_all_files_exist()
+        data_transformation_config = config.get_data_transformation_config()
+        data_transformation = DataTransformation(config=data_transformation_config)
+        data_transformation.convert()
+
+STAGE_NAME = "Data Transformation stage"
 
 if __name__ == '__main__':
     try:
         logger.info(f"*******************")
         logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-        obj = DataValidationTrainingPipeline()
+        obj = DataTransformationTrainingPipeline()
         obj.main()
         logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
     except Exception as e:
